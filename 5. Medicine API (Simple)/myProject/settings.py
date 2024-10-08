22d5af42991388f544fee054846204babc362846
rafiulislam18 <120572779+rafiulislam18@users.noreply.github.com>
2024-10-08 10:06:07 +0600

    initial commit - 5. Medicine API (Simple)

--- 5. Medicine API (Simple)/myProject/settings.py
+++ 5. Medicine API (Simple)/myProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)*hlr95a4ufyl1nr3ca#6r&3ag5uxrmf%vn#rx5vprz&m1mbc='
+SECRET_KEY = 'django-insecure-dsydza6%$_ihnlnog)(n$o53wr)snu42r&35dy2u9=@u6oju=3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'ecom_api_simple',
+    'medicine_api_simple',
 ]
 
 MIDDLEWARE = [
