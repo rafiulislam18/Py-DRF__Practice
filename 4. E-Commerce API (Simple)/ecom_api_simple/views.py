c5ebf29f1b0c95c59d33904028908d5df5ec4d19
rafiulislam18 <120572779+rafiulislam18@users.noreply.github.com>
2024-10-08 11:46:20 +0600

    completed - 4. E-Commerce API (Simple)

--- 4. E-Commerce API (Simple)/ecom_api_simple/views.py
+++ 4. E-Commerce API (Simple)/ecom_api_simple/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework.filters import SearchFilter
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+
+
+
+class ProductList(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['name', 'description']
+    pagination_class = PageNumberPagination
+
+
+
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
