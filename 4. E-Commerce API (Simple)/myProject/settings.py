3348b89ae1560364c6596e2ab5226baa26731dea
rafiulislam18 <120572779+rafiulislam18@users.noreply.github.com>
2024-10-11 11:35:52 +0600

    silly improvements

--- 4. E-Commerce API (Simple)/myProject/settings.py
+++ 4. E-Commerce API (Simple)/myProject/settings.py
@@ -38,8 +38,15 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_yasg',
     'ecom_api_simple',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,  # Set the number of items per page
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
