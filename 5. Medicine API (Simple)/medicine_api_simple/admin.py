abb3027e46959718d29ee3bd48d5ea9133e76331
rafiulislam18 <120572779+rafiulislam18@users.noreply.github.com>
2024-10-18 12:38:16 +0600

    slightly update medicine_api/admin

--- 5. Medicine API (Simple)/medicine_api_simple/admin.py
+++ 5. Medicine API (Simple)/medicine_api_simple/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Medicine
 
 # Register your models here.
+@admin.register(Medicine)
+class MedicineAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'manufacturer', 'price']
