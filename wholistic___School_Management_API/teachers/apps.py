4550aa5cb5fdc607341154e2df8f622e31a040ec
Rafiul Islam <cmmnacrafiulislam0170@gmail.com>
2024-10-22 03:38:41 +0600

    project structure completed - wholistic___School_Management_API

--- wholistic___School_Management_API/teachers/apps.py
+++ wholistic___School_Management_API/teachers/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SchoolApiWholisticConfig(AppConfig):
+class TeachersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'school_api_wholistic'
+    name = 'teachers'
