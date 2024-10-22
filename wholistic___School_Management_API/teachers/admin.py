4593d9548ed6f47a570ecc364dae3c717e9e3266
rafiulislam18 <120572779+rafiulislam18@users.noreply.github.com>
2024-10-23 04:12:12 +0600

    docs(school/teachers): update comment

--- wholistic___School_Management_API/teachers/admin.py
+++ wholistic___School_Management_API/teachers/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Teacher
 
 
-# Register Teacher model in admin with custom configuration
+# Register Teacher model with custom admin configuration
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'subject']
