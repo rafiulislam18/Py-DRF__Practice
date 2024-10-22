2c79cab0dcf06170e97f2b5f09e31bad8e990106
Rafiul Islam <cmmnacrafiulislam0170@gmail.com>
2024-10-22 20:21:04 +0600

    feat(views__teachers_app): implement function-based view for GET operation to list all Teacher data

--- wholistic___School_Management_API/teachers/views.py
+++ wholistic___School_Management_API/teachers/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Teacher
+from .serializers import TeacherSerializer
 
-# Create your views here.
+
+@api_view(['GET'])
+def teacher_list(request):
+    teachers = Teacher.objects.all()
+    serializer = TeacherSerializer(teachers, many=True)
+    return Response(serializer.data)
