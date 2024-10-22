af99b6f2406a77ddd300615e72545bef986038c6
Rafiul Islam <cmmnacrafiulislam0170@gmail.com>
2024-10-22 21:28:16 +0600

    feat(admin__school/teachers): register Teacher model to admin

--- wholistic___School_Management_API/teachers/admin.py
+++ wholistic___School_Management_API/teachers/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Teacher
 
-# Register your models here.
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'subject']
+    search_fields = ['first_name', 'last_name', 'subject']
+    list_filter = ['subject']
