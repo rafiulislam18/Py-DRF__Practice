202a54475de70c8ceefdbba3c358fa4d3cc369e8
rafiulislam18 <120572779+rafiulislam18@users.noreply.github.com>
2024-10-23 05:23:54 +0600

    feat(views__school/students): implement class-based API view for handling Student data

--- wholistic___School_Management_API/students/views.py
+++ wholistic___School_Management_API/students/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework import status
 
-# Create your views here.
+
+# API view for handling Student data
+class StudentList(APIView):
+
+    # GET method to retrieve all students list
+    def get(self, request):
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data)
+
+    # POST method to add a new student
+    def post(self, request):
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
