7f760f66bd0f3d76e86ed21c41bf2658090267ba
Rafiul Islam <cmmnacrafiulislam0170@gmail.com>
2024-10-23 21:12:01 +0600

    feat(views__school/attendance): implement generic API view for CRUD on Attendance

--- wholistic___School_Management_API/attendance/views.py
+++ wholistic___School_Management_API/attendance/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Attendance
+from .serializers import AttendanceSerializer
 
-# Create your views here.
+# API view to list & create Attendance records
+class AttendanceListCreate(generics.ListCreateAPIView):
+    queryset = Attendance.objects.all()
+    serializer_class = AttendanceSerializer
+
+# API view to retrieve, update & delete Attendance records
+class AttendanceDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Attendance.objects.all()
+    serializer_class = AttendanceSerializer
