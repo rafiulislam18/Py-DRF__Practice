13c380daa5e099ff5269193494d75ef81c5e9893
Rafiul Islam <cmmnacrafiulislam0170@gmail.com>
2024-10-23 13:23:52 +0600

    feat(admin__school/students): register Student model in admin with custom config and fieldsets

--- wholistic___School_Management_API/students/admin.py
+++ wholistic___School_Management_API/students/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Student
 
-# Register your models here.
+# Register Student model with custom admin configuration
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'class_enrolled', 'date_of_birth')
+    search_fields = ('first_name', 'last_name')
+    list_filter = ('class_enrolled',)
