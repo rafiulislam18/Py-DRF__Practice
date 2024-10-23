ae58f28786ef395cf8fcf56c56f11c7c0a8ff85c
Rafiul Islam <cmmnacrafiulislam0170@gmail.com>
2024-10-23 12:43:43 +0600

    docs(school/students): add missing comments

--- wholistic___School_Management_API/students/models.py
+++ wholistic___School_Management_API/students/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 
+# Define Student model
 class Student(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
--- wholistic___School_Management_API/students/serializers.py
+++ wholistic___School_Management_API/students/serializers.py
@@ -8,6 +8,7 @@ class StudentSerializer(serializers.ModelSerializer):
         model = Student
         fields = ['id', 'first_name', 'last_name', 'class_enrolled', 'date_of_birth']
 
+    # Custom field validation for d.o.b
     def validate_date_of_birth(self, value):
         if value.year > 2018:
             raise serializers.ValidationError("Student must be older than 6.")
