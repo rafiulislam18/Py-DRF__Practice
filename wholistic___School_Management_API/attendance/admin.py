a081cfb60e76477c3ed68533be9a3ef5d78a8dbf
Rafiul Islam <cmmnacrafiulislam0170@gmail.com>
2024-10-23 14:13:05 +0600

    feat(admin__school/attendance): register Attendance model in admin with custom config

--- wholistic___School_Management_API/attendance/admin.py
+++ wholistic___School_Management_API/attendance/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Attendance
 
-# Register your models here.
+# Register the Attendance model with custom admin configurations
+@admin.register(Attendance)
+class AttendanceAdmin(admin.ModelAdmin):
+    list_display = ['student', 'date', 'present']
+    search_fields = ['student__first_name', 'student__last_name', 'date']
+    list_filter = ['present', 'date']
