154fcd1bd27da9b8009e972c77685ef93c7a82b9
Rafiul Islam <cmmnacrafiulislam0170@gmail.com>
2024-10-23 21:32:27 +0600

    feat(urls__school/attendance): configure url for Attendance

---
+++ wholistic___School_Management_API/attendance/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import AttendanceListCreate, AttendanceDetail
+
+
+urlpatterns = [
+    # API endpoints for CRUD on Attendance
+    path('attendances/', AttendanceListCreate.as_view(), name='attendance_list_create'),
+    path('attendances/<int:pk>/', AttendanceDetail.as_view(), name='attendance_detail'),
+]
