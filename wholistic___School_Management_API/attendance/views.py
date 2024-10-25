b9bbc44b31ec0a1067b1413f357bd74c9efbee0f
rafiulislam18 <120572779+rafiulislam18@users.noreply.github.com>
2024-10-25 12:12:31 +0600

    feat(permissions__school/attendance): implement custom permission CustomIsAdminOrReadOnly on API views

--- wholistic___School_Management_API/attendance/views.py
+++ wholistic___School_Management_API/attendance/views.py
@@ -1,15 +1,18 @@
 from rest_framework import generics
 from .models import Attendance
 from .serializers import AttendanceSerializer
+from ..permissions.permissions import CustomIsAdminOrReadOnly
 
 
-# API view to list & create Attendance records
+# API view to list & create Attendance records (is admin or read-only)
 class AttendanceListCreate(generics.ListCreateAPIView):
     queryset = Attendance.objects.all()
     serializer_class = AttendanceSerializer
+    permission_classes = [CustomIsAdminOrReadOnly]
 
 
-# API view to retrieve, update & delete any Attendance record
+# API view to retrieve, update & delete any Attendance record (is admin or read-only)
 class AttendanceDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Attendance.objects.all()
     serializer_class = AttendanceSerializer
+    permission_classes = [CustomIsAdminOrReadOnly]
