2bc58a5c27b55b987791c68fea69e48966108819
rafiulislam18 <120572779+rafiulislam18@users.noreply.github.com>
2024-10-25 16:12:09 +0600

    feat(pagination_school/attendance): implement pagination on API view for Attendance

--- wholistic___School_Management_API/attendance/views.py
+++ wholistic___School_Management_API/attendance/views.py
@@ -1,14 +1,16 @@
 from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
 from .models import Attendance
 from .serializers import AttendanceSerializer
 from ..permissions.permissions import CustomIsAdminOrReadOnly
 
 
-# API view to list & create Attendance records (is admin or read-only)
+# API view to list & create Attendance records (is admin or read-only, paginated list response)
 class AttendanceListCreate(generics.ListCreateAPIView):
     queryset = Attendance.objects.all()
     serializer_class = AttendanceSerializer
     permission_classes = [CustomIsAdminOrReadOnly]
+    pagination_class = PageNumberPagination
 
 
 # API view to retrieve, update & delete any Attendance record (is admin or read-only)
