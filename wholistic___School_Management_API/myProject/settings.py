f30190c054546b99dc2a7d3aba470a8bfbde2555
rafiulislam18 <120572779+rafiulislam18@users.noreply.github.com>
2024-10-25 16:07:20 +0600

    docs(comment__school/): add & update comments

--- wholistic___School_Management_API/myProject/settings.py
+++ wholistic___School_Management_API/myProject/settings.py
@@ -53,11 +53,12 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
+    # Configure JWT authentication settings
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
 
-     # Use PageNumberPagination to paginate API responses
+     # Configure PageNumberPagination settings to paginate API responses
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
