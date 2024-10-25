2ef72909a452ee6953644324e9eed35840b23fd4
rafiulislam18 <120572779+rafiulislam18@users.noreply.github.com>
2024-10-25 12:21:49 +0600

    feat(permissions__school/teachers): implement custom permission CustomIsAdminOrReadOnly on API views

--- wholistic___School_Management_API/teachers/views.py
+++ wholistic___School_Management_API/teachers/views.py
@@ -1,11 +1,13 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from .models import Teacher
 from .serializers import TeacherSerializer
+from ..permissions.permissions import CustomIsAdminOrReadOnly
 
 
-# API view to handle GET (all) request for Teacher model
+# API view to handle GET (all) request for Teacher model (is admin or read-only)
 @api_view(['GET'])
+@permission_classes([CustomIsAdminOrReadOnly])
 def teacher_list(request):
     teachers = Teacher.objects.all()
     serializer = TeacherSerializer(teachers, many=True)
