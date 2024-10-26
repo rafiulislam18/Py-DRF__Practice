6db3f138a619a75dae5ef994da88c3661ad32a74
rafiulislam18 <120572779+rafiulislam18@users.noreply.github.com>
2024-10-26 23:01:59 +0600

    feat(models__school/classes): define Class model

--- wholistic___School_Management_API/classes/models.py
+++ wholistic___School_Management_API/classes/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+# Define Class model
+class Class(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
