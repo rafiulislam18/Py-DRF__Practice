cfed2fddcd8f830633428f7305b6052c339a980a
rafiulislam18 <120572779+rafiulislam18@users.noreply.github.com>
2024-10-27 00:11:08 +0600

    feat(school/): configure Swagger UI for API documentation

--- wholistic___School_Management_API/myProject/urls.py
+++ wholistic___School_Management_API/myProject/urls.py
@@ -17,9 +17,27 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+
+# Configure Swagger/OpenAPI schema view for School Management API documentation
+schema_view = get_schema_view(
+   openapi.Info(
+      title="School Management API",
+      default_version='v1',
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    # Configure URL for Swagger
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger'),
     
     # Configure URLs for custom apps
     path('students/', include('students.urls')),
