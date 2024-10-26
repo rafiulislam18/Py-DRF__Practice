3467509af5ae7d96b9ec2126c5dd17fc612c9441
rafiulislam18 <120572779+rafiulislam18@users.noreply.github.com>
2024-10-26 23:44:40 +0600

    feat(urls__school/classes): configure URL route for ClassViewSet

---
+++ wholistic___School_Management_API/classes/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import ClassViewSet
+
+
+# Configure URL route for ClassViewSet
+router = DefaultRouter()
+router.register(r'classes', ClassViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
