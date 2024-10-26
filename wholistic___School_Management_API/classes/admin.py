f530db1cc348c33525b3614d2655145dce9a2735
rafiulislam18 <120572779+rafiulislam18@users.noreply.github.com>
2024-10-26 23:46:09 +0600

    feat(admin__school/classes): register Class model in admin

--- wholistic___School_Management_API/classes/admin.py
+++ wholistic___School_Management_API/classes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Class
 
-# Register your models here.
+
+# Register Class model in admin
+@admin.register(Class)
+class ClassAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')  # Display these fields in the list view
+    search_fields = ('name',)  # Enable searching by name
